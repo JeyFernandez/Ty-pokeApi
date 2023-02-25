e1fb2fca51d81a64ad176fd9d6dd582c51599eee
Jasson Fernandez <jeicobfer@gmail.com>
2023-02-25 13:36:17 -0600

    Haciendo las primeras peticiones a PokeAPI con pokemon Arbok

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './style.css'
 import { arbok } from './pokemones/arbok'
+import { Pokemones } from './pokemones/arbok'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hola pokemones</h1>
   <h2>Name: ${arbok.name}</h2>
-  <h2>Name:</h2>
+  <h2>Move: </h2>
 `
 
 document.querySelector<HTMLButtonElement>('#counter')!
--- src/pokemones/arbok.ts
+++ src/pokemones/arbok.ts
@@ -1,15 +1,31 @@
 import axios from 'axios';
-import { PokeAPIResponse, Move } from '../interfaces/apiResponse.interface';
+import { PokeAPIResponse } from '../interfaces/apiResponse.interface';
 export class Pokemones{
     constructor(public id: number, public name: string){}
     
+    async getTypes(){
+        const {data} = await axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon/24');
+        console.log(`Type-> ${data.types[0].type.name}`);
+    }
+    
     async getMoves(){
         const {data} = await axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon/24');
-        console.log(data.moves[0].move.name);   
+        console.log(`Move-> ${data.moves[0].move.name}`);   
+        return data.moves
+
     }
+    async getAbilities(){
+        const {data} = await axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon/24');
+        console.log(`Avilities-> ${data.abilities[0].ability.name}`);
+    }   
 }
-export const arbok = new Pokemones(24, 'Arbok')
+export const arbok = new Pokemones(24, 'Arbok');
+//Arbok con el mentodo getMoves
 console.log(arbok.getMoves());
+//Arbok con el mentodo getTypes
+console.log(arbok.getTypes());
+//Arbok con el mentodo getTypes
+console.log(arbok.getAbilities());
 
 
 // getCharacterOfApi{
